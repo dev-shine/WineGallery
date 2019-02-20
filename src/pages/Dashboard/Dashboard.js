0eeff20b85b77ea63c6e0748a2457467845e82cb
robbieve <robbieeve751994@gmail.com>
2019-02-20 14:05:17 +1100

    [DEV-212] Add Member's badges and progress

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,42 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { compose, graphql } from 'react-apollo';
 
-import { SubscriptionSummary } from '../../components';
+import { MemberBadges, MemberProgress, SubscriptionSummary } from '../../components';
 import { GET_MEMBER } from '../../graphql/queries';
 
 import './Dashboard.scss';
 
 /**
  * Renders Dashboard component.
- * React.Component: https://reactjs.org/docs/react-component.html
  * */
-class Dashboard extends Component {
-  static propTypes = {
-    meQuery: PropTypes.shape({}).isRequired,
-  };
-
-  componentDidMount() {
-  }
-
-  render() {
-    const { meQuery } = this.props;
-    return (
-      <div className="Dashboard">
-        <div className="Dashboard--container">
-          <div className="Dashboard--forms">
-            <h1 className="Dashboard--forms__title">Your Subscription</h1>
-            <div className="Dashboard--forms__subscription-summary">
-              <SubscriptionSummary data={meQuery} />
-            </div>
+const Dashboard = props => {
+  const { meQuery } = props;
+  return (
+    <div className="Dashboard">
+      <div className="Dashboard--container">
+        <div className="Dashboard--forms">
+          <h1 className="Dashboard--forms__title">Your Subscription</h1>
+          <div className="Dashboard--forms__subscription-summary">
+            <SubscriptionSummary data={meQuery} />
           </div>
         </div>
-        This is the Dashboard component!
+        <div className="Dashboard--stats">
+          <div className="Dashboard--stats--progress">
+            <MemberProgress me={meQuery.me} />
+          </div>
+          <div className="Dashboard--stats--badges">
+            <MemberBadges me={meQuery.me} />
+          </div>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Dashboard.propTypes = {
+  meQuery: PropTypes.shape({}).isRequired,
+};
 
 export default compose(
   graphql(GET_MEMBER, { name: 'meQuery' })
